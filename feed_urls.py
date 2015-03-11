f6bd014f10e79347bd64339aa3f8c75dc280929a
James Nee <therightnee@gmail.com>
2015-03-11 16:22:07 -0400

    Update Links

--- feed_urls.py
+++ feed_urls.py
@@ -1,36 +1,37 @@
 ###Store all the urls here
 
 news_links = list([ 
-    dict(source = "Google News", locate = "http://news.google.com/news?pz=1&cf=all&ned=us&hl=en&output=rss"), 
-    dict(source = "RCP", locate = "http://feeds.feedburner.com/realclearpolitics/qlMj"),
+    dict(source = "Aggregate", locate = "http://www.rssmix.com/u/3844185/rss.xml"),
+    dict(source = "VOX", locate = "http://www.vox.com/rss/index.xml"),
     dict(source = "Audit", locate = "http://www.rssmix.com/u/3844183/rss.xml"),
     dict(source = "CATO", locate = "http://feeds.cato.org/CatoHomepageHeadlines"),
     dict(source = "CAP", locate = "http://feeds.feedburner.com/americanprogress/ydxq"),
     dict(source = "Heritage", locate = "http://blog.heritage.org/feed/"),
     dict(source = "Brookings", locate = "http://webfeeds.brookings.edu/brookingsrss/topfeeds/latestfrombrookings?format=xml"),
-    dict(source = "Aggregate", locate = "http://www.rssmix.com/u/3844185/rss.xml"),
+    dict(source = "Quartz", locate = "http://feeds.feedburner.com/qz/AYrF"),
     dict(source = "International", locate = "http://www.rssmix.com/u/3844186/rss.xml"),
-    dict(source = "Economist", locate = "http://www.rssmix.com/u/3844187/rss.xml")
+    dict(source = "538", locate = "http://fivethirtyeight.com/politics/feed/")
 ])
 
 tech_links = list([ 
     dict(source = "Gadgets", locate = "http://rssmix.com/u/3844649/rss.xml"), 
-    dict(source = "Gaming", locate = "http://rssmix.com/u/3846130/rss.xml"),
+    dict(source = "Verge", locate = "http://www.theverge.com/rss/frontpage"),
     dict(source = "Ars", locate = "http://feeds.arstechnica.com/arstechnica/index"),
     dict(source = "Toms", locate = "http://www.tomshardware.com/feeds/rss2/all.xml"),
     dict(source = "Hacker News", locate = "https://news.ycombinator.com/rss"),
     dict(source = "Web Design", locate = "http://rssmix.com/u/3844652/rss.xml"),
     dict(source = "PopSci", locate = "http://feeds.popsci.com/c/34567/f/632419/index.rss"),
     dict(source = "PopMech", locate = "http://rssmix.com/u/3844653/rss.xml"),
+    dict(source = "Fast Co", locate = "http://feeds.fastcompany.com/fastcompany/headlines"),
     dict(source = "Autos", locate = "http://rssmix.com/u/3844655/rss.xml")   
 ])
 
 biz_links = list([ 
     dict(source = "Freakonomics", locate = "http://feeds.feedburner.com/freakonomics"), 
-    dict(source = "Fast Co", locate = "http://feeds.fastcompany.com/fastcompany/headlines"),
+    dict(source = "Wisebread", locate = "http://feeds.killeraces.com/wisebread"), 
+    dict(source = "538", locate = "http://fivethirtyeight.com/economics/feed/"), 
     dict(source = "Bloomberg", locate = "http://www.bloomberg.com/feed/bview/"),
-    dict(source = "HBR", locate = "http://feeds.harvardbusiness.org/harvardbusiness?format=xml"),
-    dict(source = "Quartz", locate = "http://feeds.feedburner.com/qz/AYrF"),
+    dict(source = "HBR", locate = "http://feeds.harvardbusiness.org/harvardbusiness?fohttp://feeds.killeraces.com/wisebreadrmat=xml"),
     dict(source = "Pando Daily", locate = "http://pandodaily.com.feedsportal.com/c/35141/f/650422/index.rss"),
     dict(source = "Business Insider", locate = "http://www.businessinsider.com/rss")
 ])
@@ -45,6 +46,7 @@ religious_links = list([
 sport_links = list([ 
     dict(source = "ESPN", locate = "http://sports.espn.go.com/espn/rss/news"), 
     dict(source = "Deadspin", locate = "http://feeds.gawker.com/deadspin/full"),
+    dict(source = "538", locate = "http://fivethirtyeight.com/sports/feed/"),
     dict(source = "Great Goals", locate = "http://www.101greatgoals.com/feed/"),
     dict(source = "FBS", locate = "http://feeds.feedburner.com/fbschedulescom?format=xml")
 ])
@@ -61,8 +63,11 @@ leisure_links = list([
 ])
 
 music_links = list([ 
-    ###LINK IS DOWN dict(source = "TSiS", locate = "http://feeds.feedburner.com/thissongissick/XIcR"), 
     dict(source = "FNT", locate = "http://feeds.feedburner.com/freshnewtracks/iyTb"),
+    dict(source = "Your EDM", locate = "http://www.youredm.com/free-download/feed/"),
+    dict(source = "DA", locate = "http://dancingastronaut.com/music/download/feed/"),
+    dict(source = "Earmilk", locate = "http://feeds2.feedburner.com/earmilk"),
+    dict(source = "GVB", locate = "http://www.gorillavsbear.net/feed/"),
     dict(source = "Hype Machine", locate = "http://hypem.com/feed/popular/3day/1/feed.xml"),
     dict(source = "Pitchfork", locate = "http://rssmix.com/u/3844657/rss.xml")
 ])
