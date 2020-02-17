3d8f6cb0b9288b5148e2cefaf7a5aed59ae45865
therightnee <therightnee@gmail.com>
2020-02-17 07:37:34 -0800

    Change trigger for navigation resize to sync with media query

--- static/response.js
+++ static/response.js
@@ -1,40 +1,48 @@
- //Smartphone fix
-$( window ).resize(function() {
-  if (window.innerWidth <= 1600) 
-    $("#title").html('RR');
-    for ( var i=0; i < id_array.length; i++) {
-        $("#" + id_array[i]).empty().html(replace_letters[i]);
+// media query event handler
+if (matchMedia) {
+    const mq = window.matchMedia("(max-width: 1600px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+    }
+    
+// media query change
+function WidthChange(mq) {
+    if (mq.matches) {
+    // window width is less than 1600px
+        $("#title").html('RR');
+        for ( var i=0; i < id_array.length; i++) {
+            $("#" + id_array[i]).empty().html(replace_letters[i]);
 
-    //This is the return code
-    $(window).scroll( function() {
-      if ($(document).scrollTop() >= 100){
+            //This is the return code
+            $(window).scroll( function() {
+            if ($(document).scrollTop() >= 100){
 
-        $("#up").show();
+                $("#up").show();
 
-      }
-      else {
+            }
+            else {
+                $("#up").hide();
+            };
+            });
+
+            $("#up").click(function() {
+                $(window).scrollTop(0);
+            });
+
+            //Tap return function
+
+            $("#up").bind("tapone", function(){
+                $(window).scrollTop(0);
+            });
+        }
+    }
+    else {
+    // window width is greater than 1600px
         $("#up").hide();
-      };
-    });
-
-      $("#up").click(function() {
-        $(window).scrollTop(0);
-      });
-
-      //Tap return function
-
-      $("#up").bind("tapone", function(){
-        $(window).scrollTop(0);
-      });
-
-  };
-
-  if (window.innerWidth > 1600){
-    $("#up").hide();
-    $("#title").html('Rainbow Reader');
-    for ( var i=0; i < id_array.length; i++) {
+        $("#title").html('Rainbow Reader');
+        for ( var i=0; i < id_array.length; i++) {
         $("#" + id_array[i]).empty().html(replace_titles[i]);
-    };
-  };
+        };
+    }
 
-});
+}
