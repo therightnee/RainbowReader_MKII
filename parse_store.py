ce3b6f0014e2385060861d17c8788f5632af0e04
James Nee <therightnee@gmail.com>
2023-05-29 13:11:34 -0700

    Change Redis destination

--- parse_store.py
+++ parse_store.py
@@ -2,11 +2,12 @@ from datetime import datetime
 from time import mktime
 from dateutil.tz import tzlocal
 from feed_urls import *
+from urllib.parse import urljoin #for Redis Cloud
 import feedparser, redis, json, os, pytz
 
-
 try:
-    redis_db = redis.from_url(os.environ.get("REDIS_URL"))
+    url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+    redis_db = redis.Redis(host=url.hostname, port=url.port, password=url.password)
 except:
     print("No redis database URL set")
 
